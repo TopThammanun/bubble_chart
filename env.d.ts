85e8461a55e6a3366211b96b1a99e9921b75ea1a
SatangBudsai <satang_budsai@cmu.ac.th>
2024-04-25 23:52:41 +0700

    typescript env

---
+++ env.d.ts
@@ -0,0 +1,8 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_PROJECT_NAME: string;
+    NEXT_PUBLIC_SERVICE: string;
+    NEXT_PUBLIC_TOKEN: string;
+    NEXT_PUBLIC_ACCEPT: string;
+  }
+}
