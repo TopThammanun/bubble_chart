27aba1dfde46b32bb52e549d82c1eac326b74439
satang_budsai <satang.b@chanwanich.digital>
2024-04-26 16:30:00 +0700

    update main

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,21 @@
-import { Fragment, ReactElement } from 'react';
-import '@/styles/globals.css'
-import dynamic from 'next/dynamic'
+import { Fragment, ReactElement } from "react";
+import "@/styles/globals.css";
+import dynamic from "next/dynamic";
 
 //SetUp Store redux
 import { Provider as ReduxProvider } from "react-redux";
-import store from '@/store';
+import store from "@/store";
 
-import { AppPropsWithLayoutType } from '@/types/layout/AppPropsWithLayout';
-import NprogressProvider from '@/providers/nprogress'
-import ReactQueryProvider from '@/providers/react-query';
-import DateJSProvider from '@/providers/date';
-import NextUIProvider from '@/providers/next-ui/index';
-import AuthGuard from '@/components/auth/AuthGuard';
+import { AppPropsWithLayoutType } from "@/types/layout/AppPropsWithLayout";
+import NprogressProvider from "@/providers/nprogress";
+import ReactQueryProvider from "@/providers/react-query";
+import DateJSProvider from "@/providers/date";
+import NextUIProvider from "@/providers/next-ui/index";
+import AuthGuard from "@/providers/auth";
 
 export default function App({ Component, pageProps }: AppPropsWithLayoutType) {
   const getLayout = Component.getLayout || ((page: ReactElement) => page);
-  const auth = Component.authGuard ?? false
+  const auth = Component.authGuard ?? false;
 
   return (
     <Fragment>
@@ -33,5 +33,5 @@ export default function App({ Component, pageProps }: AppPropsWithLayoutType) {
         </ReduxProvider>
       </ReactQueryProvider>
     </Fragment>
-  )
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,101 +1,121 @@
-import { Fragment, ReactElement, useState } from 'react'
+import { Fragment, ReactElement, useState } from "react";
 import { useTheme } from "next-themes";
-import RootLayout from '@/layouts/root-layout';
-import MainLayout from '@/layouts/main-layout';
-import { DateRange } from 'react-day-picker';
-import Alert from '@/components/alert';
-import { Button, Input } from '@nextui-org/react';
-import apiBase from '@/api/base';
-import useLoaderGlobal from '@/hooks/useLoaderGlobal';
-import DatePicker from '@/components/date-picker';
-import DateMultiplePicker from '@/components/date-multiple-picker';
-import DateRangePicker from '@/components/date-range-picker';
-import Container from '@/components/container';
+import RootLayout from "@/layouts/root-layout";
+import MainLayout from "@/layouts/main-layout";
+import { DateRange } from "react-day-picker";
+import Alert from "@/components/alert";
+import { Button, Input } from "@nextui-org/react";
+import apiBase from "@/api/base";
+import useLoaderGlobal from "@/hooks/useLoaderGlobal";
+import DatePicker from "@/components/date-picker";
+import DateMultiplePicker from "@/components/date-multiple-picker";
+import DateRangePicker from "@/components/date-range-picker";
+import Container from "@/components/container";
 
-type Props = {}
+type Props = {};
 
 const Home = (props: Props) => {
-  const loaderGlobal = useLoaderGlobal()
+  const loaderGlobal = useLoaderGlobal();
 
-  const { theme, setTheme } = useTheme()
-  const [date, setDate] = useState<Date | undefined>()
-  const [arrDate, setArrDate] = useState<Date[] | undefined>()
-  const [rangeDate, setRangeDate] = useState<DateRange | undefined>()
+  const { theme, setTheme } = useTheme();
+  const [date, setDate] = useState<Date | undefined>();
+  const [arrDate, setArrDate] = useState<Date[] | undefined>();
+  const [rangeDate, setRangeDate] = useState<DateRange | undefined>();
 
   const getApi = async () => {
-    loaderGlobal.start()
-    await apiBase.get({ urlBase: 'https://randomuser.me', url: "/api" })
-    loaderGlobal.stop()
-  }
+    loaderGlobal.start();
+    await apiBase.get({ urlBase: "https://randomuser.me", url: "/api" });
+    loaderGlobal.stop();
+  };
 
   return (
     <Fragment>
-      <div className='flex flex-col gap-5'>
-        <div className='flex flex-wrap justify-center items-center gap-5'>
-          Tempalte NextJs and NextUI
+      <div className="flex flex-col gap-5">
+        <div className="flex flex-wrap justify-center items-center gap-5">
+          Template NextJs and NextUI
         </div>
 
-        <Button onClick={getApi}>Call API</Button>
+        <Button variant="flat" color="primary" onClick={getApi}>
+          Call API
+        </Button>
 
-        <div className='grid grid-cols-6 max-md:grid-cols-2 gap-5'>
-          <Button color="primary" onClick={() => (
-            Alert.message({
-              content: "Open Message",
-              noButton: true,
-            })
-          )}>
+        <div className="grid grid-cols-6 max-md:grid-cols-2 gap-5">
+          <Button
+            color="primary"
+            onClick={() =>
+              Alert.message({
+                content: "Open Message",
+                noButton: true,
+              })
+            }
+          >
             Open Message
           </Button>
 
-          <Button color="secondary" onClick={() => (
-            Alert.message({
-              content: "Open Message",
-              noButton: true,
-              color: "secondary"
-            })
-          )}>
+          <Button
+            color="secondary"
+            onClick={() =>
+              Alert.message({
+                content: "Open Message",
+                noButton: true,
+                color: "secondary",
+              })
+            }
+          >
             Open Message
           </Button>
 
-          <Button color="danger" onClick={() => (
-            Alert.error({
-              content: "Open Error",
-            })
-          )}>
+          <Button
+            color="danger"
+            onClick={() =>
+              Alert.error({
+                content: "Open Error",
+              })
+            }
+          >
             Open Error
           </Button>
 
-          <Button color="warning" onClick={() => (
-            Alert.warning({
-              content: "Open warning",
-            })
-          )}>
+          <Button
+            color="warning"
+            onClick={() =>
+              Alert.warning({
+                content: "Open warning",
+              })
+            }
+          >
             Open warning
           </Button>
 
-          <Button color="success" onClick={() => (
-            Alert.success({
-              content: "Open success",
-            })
-          )}>
+          <Button
+            color="success"
+            onClick={() =>
+              Alert.success({
+                content: "Open success",
+              })
+            }
+          >
             Open success
           </Button>
 
-          <Button color="default" onClick={() => (
-            Alert.question({
-              content: "Open Question",
-              color: 'default'
-            })
-          )}>
+          <Button
+            color="default"
+            onClick={() =>
+              Alert.question({
+                content: "Open Question",
+                color: "default",
+              })
+            }
+          >
             Open Question
           </Button>
         </div>
 
-        <div className='grid grid-cols-2 max-sm:grid-cols-1 gap-5'>
+        <div className="grid grid-cols-2 max-sm:grid-cols-1 gap-5">
           <DatePicker
             mode="single"
             label="DatePicker"
-            placeholder='Picker Date'
+            placeholder="Picker Date"
             labelPlacement="inside"
             variant="bordered"
             selected={date}
@@ -105,9 +125,9 @@ const Home = (props: Props) => {
           <DateMultiplePicker
             mode="multiple"
             label="DateMultiplePicker"
-            placeholder='DateMultiplePicker'
+            placeholder="DateMultiplePicker"
             variant="bordered"
-            captionLayout='dropdown-buttons'
+            captionLayout="dropdown-buttons"
             selected={arrDate}
             onSelect={setArrDate}
             defaultMonth={arrDate ? arrDate[0] : new Date()}
@@ -115,10 +135,10 @@ const Home = (props: Props) => {
           <DateRangePicker
             mode="range"
             label="DateRangePicker"
-            placeholder='DateMultiplePicker'
+            placeholder="DateMultiplePicker"
             labelPlacement="inside"
             variant="bordered"
-            captionLayout='dropdown-buttons'
+            captionLayout="dropdown-buttons"
             selected={rangeDate}
             onSelect={setRangeDate}
             numberOfMonths={2}
@@ -128,36 +148,34 @@ const Home = (props: Props) => {
             type="text"
             label="Name"
             placeholder="Enter Name"
-            variant='bordered'
+            variant="bordered"
           />
           <Input
             type="number"
             label="Number"
             placeholder="Enter Number"
-            variant='bordered'
+            variant="bordered"
           />
           <Input
             type="email"
             label="Email"
             placeholder="Enter Email"
-            variant='bordered'
+            variant="bordered"
           />
         </div>
       </div>
-    </Fragment >
-  )
-}
+    </Fragment>
+  );
+};
 
 Home.getLayout = (page: ReactElement) => {
   return (
     <Fragment>
       <RootLayout>
-        <MainLayout>
-          {page}
-        </MainLayout>
+        <MainLayout>{page}</MainLayout>
       </RootLayout>
     </Fragment>
   );
 };
 
-export default Home
+export default Home;
